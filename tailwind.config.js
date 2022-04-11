633808ccaf573c681b8bd64eee1f790abe5ef024
Honzze <95212442+Honzze@users.noreply.github.com>
2022-04-11 16:36:10 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  theme: {
+    screens: {
+      'xs': '475px',
+      ...defaultTheme.screens,
+    },
+  },
+  variants: {},
+  plugins: [],
+}
